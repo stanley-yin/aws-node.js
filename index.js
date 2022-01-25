86e636921438bbbd2b7527efd72fe2bf1cb73c6d
stanley Yin <stanley6140992@gmail.com>
2022-01-25 20:54:04 +0800

    修正sql連線

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "ls-d79d603c118a2aba867250d55cdd2bf276ac871d.cxkg1sljtrq7.ap-northeast-1.rds.amazonaws.com",
   user: "dbmasteruser",
+  password:'LV`n2J{g{%m0`_L2mWOJLD}_J4zegW.O',
   database: "test1",
 });
 
