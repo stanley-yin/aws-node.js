01866be59a3ee40204aab0801237fb8ef54512f0
stanley Yin <stanley6140992@gmail.com>
2022-01-25 16:53:48 +0800

    更改port為3006

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3006;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -10,10 +10,7 @@ app.get("/", (req, res) => {
   res.send(`Welcome to my server! This is the homepage on porter ${port}`);
 });
 
+
 app.get("/:name", (req, res) => {
   res.send(`Hello ${req.params.name}, welcome!`);
-});
-
-app.get("/test/", (req, res) => {
-  res.send(`if you see this you success`);
 });
