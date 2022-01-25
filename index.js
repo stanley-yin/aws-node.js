9aeb876518577b15dcb0d04eda2e0e969f037564
stanley Yin <stanley6140992@gmail.com>
2022-01-25 20:50:36 +0800

    sql-connect

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3006;
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "ls-d79d603c118a2aba867250d55cdd2bf276ac871d.cxkg1sljtrq7.ap-northeast-1.rds.amazonaws.com",
+  user: "dbmasteruser",
+  database: "test1",
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -8,8 +16,14 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.send(`Welcome to my server! This is the homepage on porter ${port}`);
+  connection.query(
+    'SELECT * FROM `test1` WHERE `user_id` = "1"',
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+  );
 });
-
 
 app.get("/:name", (req, res) => {
   res.send(`Hello ${req.params.name}, welcome!`);
